405dbbb56427a746e3519090278338b40e1199a4
Mike Holownych <mike.holownych@gmail.com>
2014-06-15 23:38:57 +0000

    Bug Fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,17 @@
 Bloccit::Application.routes.draw do
 
-  get "comments/create"
-  devise_for :users
-  resources :users, only: [:update]
+  resources :posts, only: [:index]
   resources :topics do
-    resources :posts, except: [:index] do
+    resources :posts, except: [:index], controller: 'topics/posts' do
       resources :comments, only: [:create, :destroy]
       get '/up-vote' => 'votes#up_vote', as: :up_vote
       get '/down-vote' => 'votes#down_vote', as: :down_vote
+      resources :favorites, only: [:create, :destroy]
     end
   end
+
+  devise_for :users
+  resources :users, only: [:show, :index, :update]
 
   get 'about' => 'welcome#about'
 
