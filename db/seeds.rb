e7244749ed962dd0439dca31be74ba1f7bc8c794
Mike Holownych <mike.holownych@gmail.com>
2014-06-15 19:44:26 +0000

    Implemented voting

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,77 +1,89 @@
 require 'faker'
 
-# Create 15 topics
-topics = []
+# Create Users
+5.times do
+  user = User.new(
+    name:     Faker::Name.name,
+    email:    Faker::Internet.email,
+    password: Faker::Lorem.characters(10)
+  )
+  user.skip_confirmation!
+  user.save
+end
+users = User.all
+
+
+# Create Topics
 15.times do
-  topics << Topic.create(
-    name: Faker::Lorem.words(rand(1..10)).join(" "), 
-    description: Faker::Lorem.paragraph(rand(1..4))
+  Topic.create(
+    name:         Faker::Lorem.sentence,
+    description:  Faker::Lorem.paragraph
   )
 end
- 
-
-rand(4..10).times do
-  password = Faker::Lorem.characters(10)
-  u = User.new(
-    name: Faker::Name.name, 
-    email: Faker::Internet.email, 
-    password: password, 
-    password_confirmation: password)
-  u.skip_confirmation!
-  u.save
-
-  rand(5..12).times do
-    topic = topics.first
-    p = u.posts.create(
-      topic: topic,
-      title: Faker::Lorem.words(rand(1..10)).join(" "), 
-      body: Faker::Lorem.paragraphs(rand(1..4)).join("\n"))
-    # set the created_at to a time within the past year
-    p.update_attribute(:created_at, Time.now - rand(600..31536000))
+topics = Topic.all
 
 
-    topics.rotate!
-  end
+# Create Posts
+50.times do
+  post = Post.create(
+    user:   users.sample,
+    topic:  topics.sample,
+    title:  Faker::Lorem.sentence,
+    body:   Faker::Lorem.paragraph
+  )
+
+  # set the created_at to a time within the past year
+  post.update_attribute(:created_at, rand(10.minutes .. 1.year).ago)
+  post.update_rank
+end
+posts = Post.all
+
+
+# Create Comments
+100.times do
+  comment = Comment.create(
+    user: users.sample,
+    post: posts.sample,
+    body: Faker::Lorem.paragraph
+  )
+
+  # set the created_at to a time within the past year
+  comment.update_attribute(:created_at, rand(10.minutes .. 1.year).ago)
 end
 
-post_count = Post.count
-User.all.each do |user|
-    rand(30..50).times do
-      p = Post.find(rand(1..post_count))
-      c = user.comments.create(
-        body: Faker::Lorem.paragraphs(rand(1..2)).join("\n"),
-        post: p)
-      c.update_attribute(:created_at, Time.now - rand(600..31536000))
-    end
-end
 
-u = User.new(
-  name: 'Admin User',
-  email: 'admin@example.com', 
-  password: 'helloworld', 
-  password_confirmation: 'helloworld')
-u.skip_confirmation!
-u.save
-u.update_attribute(:role, 'admin')
+# Create an admin user
+admin = User.new(
+  name:     'Admin User',
+  email:    'admin@example.com',
+  password: 'helloworld',
+  role:     'admin'
+)
+admin.skip_confirmation!
+admin.save
 
-u = User.new(
-  name: 'Moderator User',
-  email: 'moderator@example.com', 
-  password: 'helloworld', 
-  password_confirmation: 'helloworld')
-u.skip_confirmation!
-u.save
-u.update_attribute(:role, 'moderator')
+# Create a moderator
+moderator = User.new(
+  name:     'Moderator User',
+  email:    'moderator@example.com', 
+  password: 'helloworld',
+  role:     'moderator'
+)
+moderator.skip_confirmation!
+moderator.save
 
-u = User.new(
-  name: 'Member User',
-  email: 'member@example.com', 
-  password: 'helloworld', 
-  password_confirmation: 'helloworld')
-u.skip_confirmation!
-u.save
+# Create a member
+member = User.new(
+  name:     'Member User',
+  email:    'member@example.com',
+  password: 'helloworld',
+)
+member.skip_confirmation!
+member.save
+
 
 puts "Seed finished"
-puts "#{User.count} users created"
-puts "#{Post.count} posts created"
-puts "#{Comment.count} comments created"
+puts "#{User.count} Users created"
+puts "#{Topic.count} Topics created"
+puts "#{Post.count} Posts created"
+puts "#{Comment.count} Comments created"
