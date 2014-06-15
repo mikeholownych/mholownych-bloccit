23cc2a9aec2986ec33eb4ac28bf78d103b3dbc05
Mike Holownych <mike.holownych@gmail.com>
2014-06-15 15:54:28 +0000

    Destroyed topics, posts and comments.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -32,12 +32,10 @@ class CommentsController < ApplicationController
 
     if @comment.destroy
       flash[:notice] = "Comment was removed."
+      redirect_to [@topic, @post]
     else
       flash[:error] = "Comment couldn't be deleted. Try again."
-    end
-
-    respond_with(@comment) do |f|
-      f.html { redirect_to [@topic, @post] }
+      redirect_to [@topic, @post]
     end
   end
 
